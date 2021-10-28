7a5eadbb9d69eaa163d88d5581f824ddca1dbb67
cemtanrikut <cemtanrikut@outlook.com>
2021-10-28 17:32:35 +0300

    Update time formatter

--- helper/time-formatter.go
+++ helper/time-formatter.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func FormatTime(date string) string {
+func FormatTime(date string) (time.Time, error) {
 	layout := "2006-01-02"
 	t, err := time.Parse(layout, date)
 	if err != nil {
@@ -13,7 +13,15 @@ func FormatTime(date string) string {
 	}
 	//Format ISO 8601 (RFC 3339)
 	//Like this: 2017-01-28T01:22:14.398+00:00
-	newTime := t.Format(time.RFC3339)
+	formattedTime := t.Format(time.RFC3339)
+	fmt.Println(formattedTime)
 
-	return newTime
+	const timeLayout = "2006-01-02T15:04:05Z"
+
+	newTime, err := time.Parse(timeLayout, formattedTime)
+	if err != nil {
+		fmt.Println("Time parsing err ", err)
+	}
+
+	return newTime, err
 }
