f817f5f7ef2b3b6658a198cc5489ffed66c98a8e
cemtanrikut <cemtanrikut@outlook.com>
2021-10-28 12:01:54 +0300

    Add response helper

---
+++ helper/response.go
@@ -0,0 +1,24 @@
+package helper
+
+type Resp struct {
+	Success bool        `json:"success"`
+	Method  string      `json:"method"`
+	Action  string      `json:"action,omitempty"`
+	Data    interface{} `json:"data"`
+}
+
+// NewSuccess returns `*Success` instance.
+func NewResponse() *Resp {
+	return &Resp{}
+}
+
+func ResponseSuccess(success bool, action string, data interface{}, method string) (httpResp *Resp) {
+
+	httpResp = NewResponse()
+	httpResp.Success = success
+	httpResp.Action = action
+	httpResp.Data = data
+	httpResp.Method = method
+	return httpResp
+
+}
