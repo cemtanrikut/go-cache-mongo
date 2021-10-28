de8a3dc2513954160b951fbf95f80e574f866dad
cemtanrikut <cemtanrikut@outlook.com>
2021-10-28 11:58:04 +0300

    Add models

---
+++ model/data.go
@@ -0,0 +1,6 @@
+package model
+
+type KeyValData struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
---
+++ model/response-data.go
@@ -0,0 +1,13 @@
+package model
+
+type ResponseData struct {
+	Code    int               `json:"code"`
+	Msg     string            `json:"msg"`
+	Records map[Record]Record `json:"records"`
+}
+
+type Record struct {
+	Key        string `json:"key"`
+	CreatedAt  string `json:"createdAt"`
+	totalCount int    `json:"totalCount"`
+}
